1c4fd5ae906a5648d70b7df0ac2572282da6f9c6
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-08-13 01:08:12 +0100

    rustfmt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,8 @@
 extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -11,22 +11,23 @@ fn main() {
 
     println!("The secret number is: {}.", secret_number);
 
-    loop{
+    loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse(){
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number){
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             _ => break,
